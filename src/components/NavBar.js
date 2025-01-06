9bc472081728d1f996c1e492efbfa13935406684
rawhx_x <achmadhasbil24@gmail.com>
2025-01-06 19:55:35 +0700

    update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons"
 import { faBars } from "@fortawesome/free-solid-svg-icons/faBars"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { GlobalContext } from "../global/context"
 
 const NavBar = ({activeSection}) => {
-    const [hamburgerActive, setHamburgerActive] = useState(false)
+    const { hamburger } = useContext(GlobalContext)
     
     function scrollToSection(sectionId) {
         const element = document.getElementById(sectionId);
@@ -21,10 +22,13 @@ const NavBar = ({activeSection}) => {
             navbar.classList.remove('navbar-scrolled');
         }
     });
+
+    const menu = ["home", "journey", "contact"]
     
     return (
         <>
-            <div className="h-screen fixed z-40 w-full"  onClick={hamburgerActive ? () => setHamburgerActive(!hamburgerActive) : undefined}>
+            {/* <div className=""  onClick={hamburgerActive ? () => setHamburgerActive(!hamburgerActive) : undefined}> */}
+            <div className="" >
                 <div id="navbar" className="px-5 h-min-12 fixed z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] supports-backdrop-blur:bg-white/100 dark:bg-transparent">
                     <div className="flex flex-row justify-between items-center py-3">
                         <div>
@@ -34,47 +38,33 @@ const NavBar = ({activeSection}) => {
                             </button>
                         </div>
                         <div className="hidden md:flex flex-row gap-5">
-                            <button 
-                                className={`nav-link ${activeSection === "home" ? 'nav-link-active' : ''}`}
-                                onClick={() => scrollToSection('home')}>
-                                Home
-                            </button>
-
-                            <button 
-                                className={`nav-link ${activeSection === "about" ? 'nav-link-active' : ''}`}
-                                onClick={() => scrollToSection('about')}>
-                                Journey
-                            </button>
-
-                            <button 
-                                className={`nav-link ${activeSection === "contact" ? 'nav-link-active' : ''}`}
-                                onClick={() => scrollToSection('contact')}>
-                                Contact
-                            </button>
+                            {
+                                menu.map((data, index) => (
+                                    <button 
+                                        key={index}
+                                        className={`capitalize nav-link ${activeSection === data ? 'nav-link-active' : ''}`}
+                                        onClick={() => scrollToSection(data)}>
+                                        {data}
+                                    </button>
+                                ))
+                            }
                         </div>
-                        <button onClick={()=>setHamburgerActive(!hamburgerActive)} className="md:hidden">
-                            <FontAwesomeIcon icon={hamburgerActive ? faXmark : faBars}  className={`hamburger transform ${hamburgerActive ? 'rotate-90' : 'rotate-0'}`}  size="xl" />
+                        <button onClick={()=>hamburger.setHamburgerActive(!hamburger.hamburgerActive)} className="md:hidden">
+                            <FontAwesomeIcon icon={hamburger.hamburgerActive ? faXmark : faBars}  className={`hamburger transform ${hamburger.hamburgerActive ? 'rotate-90' : 'rotate-0'}`}  size="xl" />
                         </button>        
                     </div>
                 
-                        <div className={`${hamburgerActive ? 'menu-open' : 'menu-closed'} flex flex-col gap-5 justify-center items-center py-3 transition-all duration-500 ease-in-out`}>
-                            <button 
-                                className={`nav-link ${activeSection === "home" ? 'nav-link-active' : ''}`}
-                                onClick={() => scrollToSection('home')}>
-                                Home
-                            </button>
-
-                            <button 
-                                className={`nav-link ${activeSection === "about" ? 'nav-link-active' : ''}`}
-                                onClick={() => scrollToSection('about')}>
-                                Journey
-                            </button>
-
-                            <button 
-                                className={`nav-link ${activeSection === "contact" ? 'nav-link-active' : ''}`}
-                                onClick={() => scrollToSection('contact')}>
-                                Contact
-                            </button>
+                        <div className={`${hamburger.hamburgerActive ? 'menu-open' : 'menu-closed'} flex flex-col gap-5 justify-center items-center py-3 transition-all duration-500 ease-in-out`}>
+                            {
+                                menu.map((data, index) => (
+                                    <button 
+                                        key={index}
+                                        className={`capitalize nav-link ${activeSection === data ? 'nav-link-active' : ''}`}
+                                        onClick={() => scrollToSection(data)}>
+                                        {data}
+                                    </button>
+                                ))
+                            }
                         </div>
                 </div>      
             </div>
